6cafee9dd2e071c047128eecef2fe098e982d29d
jbgeorge98@yahoo.com <geojob@batComp.localdomain>
2022-07-31 10:18:26 -0700

    Imports and main

---
+++ src/litho.py
@@ -0,0 +1,54 @@
+import os
+import sys 
+import skimage as ski
+import numpy as np
+from stl import mesh
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def convImgStl():
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    import sys
+    convImgStl(sys.argv[2])
